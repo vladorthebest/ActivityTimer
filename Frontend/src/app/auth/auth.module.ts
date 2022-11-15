7790478e2414e434cc27919ffa4d2ad410db1419
Vladyslav Nosov <vladorthebest@gmail.com>
2022-11-15 21:51:32 +0100

    Create Login component (Day 14)

--- Frontend/src/app/auth/auth.module.ts
+++ Frontend/src/app/auth/auth.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  imports: [CommonModule, AuthRoutingModule, RouterModule],
   exports: [AuthRoutingModule, RouterModule],
 })
 export class AuthModule {}
