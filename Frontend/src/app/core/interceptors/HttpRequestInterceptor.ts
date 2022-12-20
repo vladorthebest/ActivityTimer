fab2d849ee9d0753cc6f7f77a9f4cdefb5bd6d65
Vladyslav Nosov <vladorthebest@gmail.com>
2022-12-20 19:20:30 +0100

    Add cookie in interceptor

--- Frontend/src/app/core/interceptors/HttpRequestInterceptor.ts
+++ Frontend/src/app/core/interceptors/HttpRequestInterceptor.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+    constructor (private cookieService: CookieService){}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         req = req.clone({
             withCredentials: true,
+            headers: req.headers.set('X-CSRFToken', this.cookieService.get('csrftoken'))
         });
 
         return next.handle(req);
